c2729f90a916f8a02f5f79030c8316f0e8d5a075
rian27Kapllanaj <arian27kapllanaj@gmail.com>
2021-06-01 22:12:50 +0200

    adding google map

--- app/src/main/java/com/example/medicinereminder/MainActivity.java
+++ app/src/main/java/com/example/medicinereminder/MainActivity.java
@@ -21,4 +21,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(), Login.class));
         finish();
     }
+
+    public void map(View view) {
+        startActivity(new Intent(getApplicationContext(), GoogleMap.class));
+        finish();
+    }
 }
